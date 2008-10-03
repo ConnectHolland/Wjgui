dfd8286152a35b9e0d0c940fa0a8b8cca1d4c5af
Giso Stallenberg <giso@connectholland.nl>
2008-10-03 14:15:27 +0000

    WJGui: made settings object usefull
    git-svn-id: file:///srv/svn/main/libraries/wjgui/trunk@23446 3e26f76b-fb05-0410-91fc-a0551e8366c8

--- WJGuiSettings.js
+++ WJGuiSettings.js
@@ -1,3 +1,13 @@
-var WJGuiSettings = {
-	windowBaseTitle = "Windmill CMS"
-}
+/**
+ * WJGuiSettings
+ *
+ * Mixes existing WJGuiSettings with default values
+ *
+ * @since Fri Oct 3 2008
+ * @revision $Revision$
+ * @author Giso Stallenberg
+ * @package Windmill.WJGui.Javascript
+ **/
+var WJGuiSettings = Object.extend({
+	windowBaseTitle: "Windmill CMS"
+}, WJGuiSettings || {});
